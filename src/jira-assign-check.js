8e5806461d770226109eceea90ebdc13f925035b
preacherlemon <preacherlemon@outlook.com>
2021-05-15 17:50:52 +0530

    will a delay help

--- src/jira-assign-check.js
+++ src/jira-assign-check.js
@@ -54,15 +54,15 @@ async function run(){
         //This block checks for assignment
         try{
             if(issueDetails.fields.assignee.accountId!=null){
-                console.log("JIRA Review - Assignment Check PASSED");
-                resultMessages.push("\n JIRA Review - `Assignment` Check PASSED ✅");
+                console.log("Assignment Check PASSED");
+                resultMessages.push("\n `Assignment` Check PASSED ✅");
                 reviewResult = reviewResult+1;
                 core.setOutput("jira-assigned",true);
             }
         }
         catch(err){
-            console.log("JIRA Review - Assignment Check FAILED");
-            resultMessages.push("\n JIRA Review - `Assignment` Check FAILED ❌");
+            console.log("Assignment Check FAILED");
+            resultMessages.push("\n `Assignment` Check FAILED ❌");
             core.setOutput("jira-assigned",false);
         }
 
@@ -77,93 +77,94 @@ async function run(){
                 }
             }
             if(issueDetails.fields[sprintFieldId]!=null){
-                console.log("JIRA Review - Sprint Check PASSED");
-                resultMessages.push("\n JIRA Review - `Sprint` Check PASSED ✅");
+                console.log("Sprint Check PASSED");
+                resultMessages.push("\n `Sprint` Check PASSED ✅");
                 reviewResult = reviewResult+1;
                 core.setOutput("jira-sprint",true);
             }else{
-                console.log("JIRA Review - Sprint Check FAILED");
-                resultMessages.push("\n JIRA Review - `Sprint` Check FAILED ❌");
+                console.log("Sprint Check FAILED");
+                resultMessages.push("\n `Sprint` Check FAILED ❌");
                 core.setOutput("jira-sprint",false);
             }
         });
 
-        //This block checks for whether fix version is present
-        if(issueDetails.fields.fixVersions.length!=0){
-            console.log("JIRA Review - Fix version Check PASSED");
-            resultMessages.push("\n JIRA Review - `Fix version` Check PASSED ✅");
-            reviewResult = reviewResult+1;
-            core.setOutput("jira-fixversion",true);
-        }else{
-            console.log("JIRA Review - Fix version Check FAILED");
-            resultMessages.push("\n JIRA Review - `Fix version` Check FAILED ❌");
-            core.setOutput("jira-fixversion",false);
-        }
-
-        //This block checks for whether time is logged
-        if(issueDetails.fields.timespent!=null){
-            console.log("JIRA Review - Time Logging Check PASSED");
-            resultMessages.push("\n JIRA Review - `Time logging` Check PASSED ✅");
-            reviewResult = reviewResult+1;
-            core.setOutput("jira-timelogging",true);
-        }else{
-            console.log("JIRA Review - Time Logging Check FAILED");
-            resultMessages.push("\n JIRA Review - `Time Logging` Check FAILED ❌");
-            core.setOutput("jira-timelogging",false);
-        }
-
-        //This block checks for status
-        if(issueDetails.fields.status.name=="In Progress"){
-            console.log("JIRA Review - Issue Status is In Progress");
-            resultMessages.push("\n JIRA Review - `Issue Status` Check PASSED ✅");
-            reviewResult = reviewResult+1;
-            core.setOutput("jira-status",true);
-        }else{
-            console.log("JIRA Review - Issue Status is "+issueDetails.fields.status.name+". It must be In Progress when the PR is open.");
-            resultMessages.push("\n JIRA Review - `Issue Status` Check FAILED ❌");
-            core.setOutput("jira-status",false);
-        }
+        setTimeout(()=>
+        {
+                if(issueDetails.fields.fixVersions.length!=0){
+                    console.log("Fix version Check PASSED");
+                    resultMessages.push("\n `Fix version` Check PASSED ✅");
+                    reviewResult = reviewResult+1;
+                    core.setOutput("jira-fixversion",true);
+                }else{
+                    console.log("Fix version Check FAILED");
+                    resultMessages.push("\n `Fix version` Check FAILED ❌");
+                    core.setOutput("jira-fixversion",false);
+                }
         
-        if(reviewResult==5){
-            console.log("All Checks PASSED");
-            finalMessage.push("\n JIRA Review Report: All Checks PASSED ✅");
-            core.setOutput("result",true);
-        }else{
-            console.log("Some Checks have FAILED.");
-            finalMessage.push("\n JIRA Review Report: Some Checks FAILED ❌");
-            core.setOutput("result",false);
-        }
-
-        const pull_request_number = github.context.payload.pull_request.number;
-        var details_message = "\n\
-### JIRA Details: \n\n\
-| JIRA Issue ID |["+issueDetails.key+"]("+issueDetails.self+")| \n\
-|-|-| \n\
-| JIRA Summary |"+issueDetails.fields.summary+"| \n\
-| JIRA Issue Type | "+issueDetails.fields.issuetype.name+"| \n\
-| JIRA Status |"+issueDetails.fields.status.name+"|\n\
-"
-        var results_message = "\n\
-### JIRA Review Checks \n\
-"+resultMessages+"\n\
-"
-
-        var results_report = "\n\
-### JIRA Review Results \n\
-"+finalMessage+"\n\
-"
-
-        var message = details_message + results_message + results_report;
-
-        const new_comment = octokit.rest.issues.createComment({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            issue_number: pull_request_number,
-            body: message
-            });
-
-    }
-    );
+                //This block checks for whether time is logged
+                if(issueDetails.fields.timespent!=null){
+                    console.log("Time Logging Check PASSED");
+                    resultMessages.push("\n `Time logging` Check PASSED ✅");
+                    reviewResult = reviewResult+1;
+                    core.setOutput("jira-timelogging",true);
+                }else{
+                    console.log("Time Logging Check FAILED");
+                    resultMessages.push("\n `Time Logging` Check FAILED ❌");
+                    core.setOutput("jira-timelogging",false);
+                }
+        
+                //This block checks for status
+                if(issueDetails.fields.status.name=="In Progress"){
+                    console.log("Issue Status is In Progress");
+                    resultMessages.push("\n `Issue Status` Check PASSED ✅");
+                    reviewResult = reviewResult+1;
+                    core.setOutput("jira-status",true);
+                }else{
+                    console.log("Issue Status is "+issueDetails.fields.status.name+". It must be In Progress when the PR is open.");
+                    resultMessages.push("\n `Issue Status` Check FAILED ❌");
+                    core.setOutput("jira-status",false);
+                }
+                
+                if(reviewResult==5){
+                    console.log("All Checks PASSED");
+                    finalMessage.push("\n JIRA Review Report: All Checks PASSED ✅");
+                    core.setOutput("result",true);
+                }else{
+                    console.log("Some Checks have FAILED.");
+                    finalMessage.push("\n JIRA Review Report: Some Checks FAILED ❌");
+                    core.setOutput("result",false);
+                }
+        
+                const pull_request_number = github.context.payload.pull_request.number;
+                var details_message = "\n\
+        ### JIRA Details: \n\n\
+        | JIRA Issue ID |["+issueDetails.key+"]("+issueDetails.self+")| \n\
+        |-|-| \n\
+        | JIRA Summary |"+issueDetails.fields.summary+"| \n\
+        | JIRA Issue Type | "+issueDetails.fields.issuetype.name+"| \n\
+        | JIRA Status |"+issueDetails.fields.status.name+"|\n\
+        "
+                var results_message = "\n\
+        ### JIRA Review Checks \n\
+        "+resultMessages+"\n\
+        "
+        
+                var results_report = "\n\
+        ### JIRA Review Results \n\
+        "+finalMessage+"\n\
+        "
+        
+                var message = details_message + results_message + results_report;
+        
+                const new_comment = octokit.rest.issues.createComment({
+                    owner: github.context.repo.owner,
+                    repo: github.context.repo.repo,
+                    issue_number: pull_request_number,
+                    body: message
+                    });
+        
+        });
+    });        
 }
 
 run();
